deae0f7ed54ab4f4f430195cae30caab0232180a
waabbajak <waabbajak@gmail.com>
2019-02-07 20:15:24 -0700

    Initial Commit

---
+++ MegaDesk_1.1_JamesKennedy/MegaDesk_1.1_JamesKennedy/Desk.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaDesk_3_JamesKennedy
+{
+    class Desk
+    {
+        public int Width { get; set; }
+        public int Depth { get; set; }
+        public int NumberOfDrawers { get; set; }
+        public DesktopMaterial DesktopMaterial { get; set; }
+        
+
+        // Constraints
+        public const int MINWIDTH = 24;
+        public const int MAXWIDTH = 96;
+        public const int MAXDEPTH = 48;
+        public const int MINDEPTH = 12;
+        public const int MINDRAWERS = 0;
+        public const int MAXDRAWERS = 7;
+    }
+
+    public enum DesktopMaterial
+    {
+        Oak = 200,
+        Laminate = 100,
+        Pine = 50,
+        Rosewood = 300,
+        Veneer = 125
+    }
+}
---
+++ MegaDesk_1.1_JamesKennedy/MegaDesk_1.1_JamesKennedy/SearchQuotes.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MegaDesk_3_JamesKennedy
+{
+    public partial class SearchQuotes : Form
+    {
+        DesktopMaterial mats;
+
+        public SearchQuotes()
+        {
+            InitializeComponent();
+            // list to populate comboBox
+            List<DesktopMaterial> MatBoxList = Enum.GetValues(typeof(DesktopMaterial))
+                .Cast<DesktopMaterial>().ToList();
+
+            MatComboBox.DataSource = MatBoxList;
+        }
+
+        private void SearchQuotes_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string MaterialSelected = MatComboBox.SelectedItem.ToString();
+
+                using (StreamReader sr = new StreamReader(@"quotes.txt"))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        // string[] value = sr.ReadLine().Split(',');
+                        if (sr.ReadLine().Contains(MaterialSelected))
+                        {
+                            // add to output ListView 
+                            ResultsView.Columns.Add(sr.ReadLine());
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
